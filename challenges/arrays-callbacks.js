dcbd8cd390b9d422b034b5241a91406dd79b8891
boahs <39522778+boahs@users.noreply.github.com>
2020-07-24 11:36:51 -0400

    adding .reduce()

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -51,10 +51,16 @@ console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: a callback (which itself takes two args), 
+and an initial value for the count.
 
 */
 let populationTotal = 0;
+
+zooAnimals.reduce((acum, reducedPop) => populationTotal = acum + reducedPop.population,0) //remember to add index
+
 console.log(populationTotal);
 
 
