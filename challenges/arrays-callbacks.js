1fcbbcc3c0272dc156c03003da3466d3f5636afb
boahs <39522778+boahs@users.noreply.github.com>
2020-07-24 11:26:33 -0400

    adding task 3 .filter() < 5

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -41,10 +41,12 @@ console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+The zoos are concerned about animals with a lower population count. Using filter, 
+create a new array of objects called lowPopulationAnimals which contains only the 
+animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = zooAnimals.filter(lowPop => lowPop.population < 5)
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
